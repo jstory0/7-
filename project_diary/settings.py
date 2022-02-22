17705a5758ea0dc89cd4b4f1fcaac1b469bd4c71
yuyoungjae <yuyj97@naver.com>
2022-02-22 09:50:25 +0900

    second commit

--- project_diary/settings.py
+++ project_diary/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mydiary.apps.MydiaryConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'project_diary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_diary_db',
+        'USER': 'root',
+        'PASSWORD': 'yu974346!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
--- project_diary/urls.py
+++ project_diary/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
+    path('',TemplateView.as_view(template_name='main.html'),name='home'),
     path('admin/', admin.site.urls),
 ]
